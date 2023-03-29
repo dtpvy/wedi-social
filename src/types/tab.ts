edbe575b01874f7d71561cfc1e847ee2f87308b7
MichaMichaHii <minhchanhwork@gmail.com>
2023-03-29 20:07:30 +0700

    merge feed + main

--- src/types/tab.ts
+++ src/types/tab.ts
@@ -1,17 +1,8 @@
-<<<<<<< HEAD
-import { ImageProps } from "@mantine/core";
-import { ImgHTMLAttributes } from "react";
-=======
 import { ReactNode } from "react";
->>>>>>> main
 
 export type Tab = {
   name: string;
   title?: string;
   url: string;
-<<<<<<< HEAD
-  icon : string; 
-=======
   icon?: ReactNode;
->>>>>>> main
 };
