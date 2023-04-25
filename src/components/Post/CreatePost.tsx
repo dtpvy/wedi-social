95f5b2f23519a0207fe8760d5c0cbc75fbc4008e
NDPHAP <phap1788@gmail.com>
2023-04-25 21:07:02 +0700

    upload translate feed/feed

--- src/components/Post/CreatePost.tsx
+++ src/components/Post/CreatePost.tsx
@@ -1,19 +1,22 @@
 import { Avatar, Button, Input } from "@mantine/core";
 import { IconMapPinFilled, IconSearch } from "@tabler/icons-react";
 import React from "react";
+import useTranslation from "@/hooks/useTranslation";
 
 const CreatePost = () => {
+  const {t} = useTranslation();
+
   return (
     <div className="bg-white shadow p-4 rounded ">
       <div className="flex items-center gap-4 w-full">
         <Avatar radius="xl" />
-        <Input placeholder="Write something..." radius="xl" className="w-1/2" />
+        <Input placeholder={t("statusText")} radius="xl" className="w-1/2" />
         <div className="flex items-center gap-1 ml-auto">
           <IconMapPinFilled className="text-red-600" size={30} />
-          <div>Chọn địa điểm</div>
+          <div>{t("selectLocationText")}</div>
         </div>
         <Button color="green" radius="xl">
-          Đăng bài
+          {t("postVerbText")}
         </Button>
       </div>
     </div>
